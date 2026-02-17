9bee679056f0cc5a02b216f55d568d552c849a83
pooriya <pooriya.adib.engineer@gmail.com>
2026-02-17 20:29:44 +0330

    the telemetry model be created

--- iot_device_management/telemetry/models.py
+++ iot_device_management/telemetry/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from devices.models import Device
 
-# Create your models here.
+
+class TelemetryData(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='telemetry')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    metric_name = models.CharField(max_length=50)
+    metric_value = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.device.name} - {self.metric_name} @ {self.timestamp}"
