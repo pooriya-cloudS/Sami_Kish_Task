391884949a2e580f107d234cb9450d0a81afe3fb
pooriya <pooriya.adib.engineer@gmail.com>
2026-02-17 19:30:01 +0330

    the code style be fixed

--- iot_device_management/iot_device_management/wsgi.py
+++ iot_device_management/iot_device_management/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iot_device_management.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iot_device_management.settings")
 
 application = get_wsgi_application()
