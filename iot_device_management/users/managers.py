04ad0244e90f0a1c038be9b64c60c62ae862942c
pooriya <pooriya.adib.engineer@gmail.com>
2026-02-17 20:15:23 +0330

    the custome user model be created

---
+++ iot_device_management/users/managers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("An email is required.")
+
+        user = self.model(
+            email=email
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, password, email, name):
+        user = self.model(
+            email=email,
+            name=name,
+        )
+        user.set_password(password)
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(using=self._db)
+        return user
--- iot_device_management/users/models.py
+++ iot_device_management/users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from . import managers
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=200, unique=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    update_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expire_at = models.DateTimeField(null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ["name"]
+    objects = managers.UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def get_full_name(self):
+        return f"{self.name}"
