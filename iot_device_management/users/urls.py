24405a2b15ddbea525b29ed156055b49ef1699ba
pooriya <pooriya.adib.engineer@gmail.com>
2026-02-19 09:35:12 +0330

    the urls issue be fixed

--- iot_device_management/users/urls.py
+++ iot_device_management/users/urls.py
@@ -4,6 +4,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("users/", UserListCreateView.as_view(), name="user-list-create"),
-    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
