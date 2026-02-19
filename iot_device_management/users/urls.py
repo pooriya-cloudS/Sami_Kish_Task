6913177c5f7462d3cc95661f770293b5e9a20948
pooriya <pooriya.adib.engineer@gmail.com>
2026-02-19 04:07:50 +0330

    the customer api`s be created

---
+++ iot_device_management/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import UserListCreateView
+
+urlpatterns = [
+    path("users/", UserListCreateView.as_view(), name="user-list-create"),
+]
+
+
--- iot_device_management/users/views.py
+++ iot_device_management/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from drf_spectacular.utils import extend_schema
+from .models import User
+from .serializers import UserSerializer
+from iot_device_management.paginations import StandardPagePagination
 
-# Create your views here.
+
+@extend_schema(tags=["Users"])
+class UserListCreateView(APIView):
+    pagination_class = StandardPagePagination
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def get(self, request):
+        users = User.objects.all()
+        paginator = self.pagination_class()
+        paginated_users = paginator.paginate_queryset(users, request)
+        serializer = self.serializer_class(paginated_users, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
